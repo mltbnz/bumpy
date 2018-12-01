a0a9b470ad2b7bf4f07e08da1e09e39605e42598
Malte Buenz <malte.buenz@deutschebahn.com>
2018-12-01 14:12:36 +0100

    initial commit 🎉

---
+++ bumpy/Sources/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  bumpy
+//
+//  Created by Malte Bünz on 06.11.18.
+//  Copyright © 2018 mbnz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let controller = BumpyController()
+    }
+
+
+}
+
